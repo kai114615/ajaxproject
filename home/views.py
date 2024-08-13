d43eaf146f327e622c530a6913de6f981039eaea
kai114615 <peter82615@yahoo.com.tw>
2024-08-13 17:23:09 +0800

    複習到20240810-讀json資料顯示在網頁表格上

--- home/views.py
+++ home/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    title = 'async await'
+    return render(request, 'home/index.html', locals())
+
+def json_demo(request):
+    title = 'JSON 練習'
+    return render(request, 'home/json.html', locals())
