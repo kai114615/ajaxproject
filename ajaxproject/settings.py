0f8f1b2af31a98840348531831b7327556b979fe
kai114615 <peter82615@yahoo.com.tw>
2024-08-13 17:50:27 +0800

    settings 修改為 mydb 資料庫

--- ajaxproject/settings.py
+++ ajaxproject/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'ajaxproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'kai114615',
+        # 'PASSWORD': 'Kai114615',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
